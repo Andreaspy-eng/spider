1a69eec535498a045dab95750bf8472bc15f1413
Andrew <foki.andrew@gmail.com>
2023-07-07 02:02:47 +0300

    temporary solution instead of method in api (ShipmentDate)

--- spider.Application/AdvantageService.cs
+++ spider.Application/AdvantageService.cs
@@ -28,24 +28,28 @@ namespace spider
             _advantageClient = client;
             _advantageClient.BaseAddress = new Uri(_config["Advantage:Base"]);
         }
+        // Send to Yandex
         public IEnumerable<InvoiceHeader> getInvoices()
         {
-            string thisDay = DateTime.UtcNow.ToString("yyyy-MM-dd");
-            string beforeDay = DateTime.UtcNow.AddDays(-1).ToString("yyyy-MM-dd");
+            DateTime thisDay = DateTime.UtcNow;
+            string beforeDay = thisDay.AddDays(-5).ToString("yyyy-MM-dd");
 
-            using (Stream s = _advantageClient.GetStreamAsync(_config["Advantage:Invoices"] + $"?Start={beforeDay}&End={thisDay}&Codes=Т-р&Codes=Т-Р").Result)
+            using (Stream s = _advantageClient.GetStreamAsync(_config["Advantage:Invoices"] + $"?Start={beforeDay}&End={thisDay.ToString("yyyy-MM-dd")}&Codes=Т-р&Codes=Т-Р").Result)
             using (StreamReader sr = new StreamReader(s))
             using (JsonReader reader = new JsonTextReader(sr))
             {
                 JsonSerializer serializer = new();
                 var InvoiceList = serializer.Deserialize<IEnumerable<InvoiceHeader>>(reader);
-                return InvoiceList;
+                return InvoiceList.Where(x => 
+                  x.ShipmentDate.ToString("yyyy-MM-dd")==(thisDay.AddDays(1).ToString("yyyy-MM-dd"))).ToList();
             };
         }
+
+        // Get by date from Yandex result
         public IEnumerable<InvoiceHeader> getInvoices(string thisDay)
         {
           DateTime dResult = DateTime.ParseExact(thisDay, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-          string beforeDay = dResult.AddDays(-1).ToString("yyyy-MM-dd");
+          string beforeDay = dResult.AddDays(-5).ToString("yyyy-MM-dd");
 
             using (Stream s = _advantageClient.GetStreamAsync(_config["Advantage:Invoices"] + $"?Start={beforeDay}&End={thisDay}&Codes=Т-р&Codes=Т-Р").Result)
             using (StreamReader sr = new StreamReader(s))
@@ -53,7 +57,8 @@ namespace spider
             {
                 JsonSerializer serializer = new();
                 var InvoiceList = serializer.Deserialize<IEnumerable<InvoiceHeader>>(reader);
-                return InvoiceList;
+                return InvoiceList.Where(x => 
+                  x.ShipmentDate.ToString("yyyy-MM-dd")==(dResult.AddDays(1).ToString("yyyy-MM-dd"))).ToList();
             };
         }
         public IEnumerable<Driver> GetDrivers()
