5fbe5ff5e84652b8e1c600a678131b448a1d3f11
Andrew <foki.andrew@gmail.com>
2023-07-20 14:48:22 +0300

    few views edit

--- spider.Web/wwwroot/site.js
+++ spider.Web/wwwroot/site.js
@@ -5,7 +5,7 @@ function SetAllCheckBoxes(obj)
     var c = new Array();
     var cards = document.querySelectorAll('.show');
     if (cards.length < 1) {
-        c = document.getElementsByName("checkboxes");
+        c = document.getElementsByName("checkboxes-cars");
     }
     else {
         for (var i = 0; i < cards.length; i++) {
@@ -43,37 +43,79 @@ document.getElementById('search-box').addEventListener("keyup", function()
 
 {
   t = document.getElementById('search-box'); 
-  let text = t.value.toUpperCase();          
-  c = document.querySelectorAll("[id^='car-model']")
+  let text = t.value.toUpperCase();
+  // name="checkboxes-cars"       
+  c = document.querySelectorAll("[id^='car-number']")
+  d = document.querySelectorAll("[id^='car-model']")
+
   for (var i = 0; i < c.length; i++) {
-    a = c[i]
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(text) > -1) {
-      a.parentElement.parentElement.parentElement.style.display = "";
+    number = c[i]
+    model = d[i]
+    txtValue = number.textContent || number.innerText;
+    modelValue = model.textContent || model.innerText;
+    if (txtValue.toUpperCase().indexOf(text) > -1 || modelValue.toUpperCase().indexOf(text) > -1 ) {
+      number.parentElement.parentElement.parentElement.style.display = "";
     } else {
-      a.parentElement.parentElement.parentElement.style.display = "none";
+      number.parentElement.parentElement.parentElement.style.display = "none";
     }
   }
 })
 
 // For Search invoice on Index page
-document.getElementById('search-box').addEventListener("keyup", function()
+document.getElementById('search-invoices').addEventListener("keyup", function()
 
 {
   t = document.getElementById('search-invoices'); 
   let text = t.value.toUpperCase();          
-  c = document.querySelectorAll("[id^='invoice']")
+  c = document.querySelectorAll("[id^='counterparty-name']")
+  d = document.querySelectorAll("[id^='counterparty-address']")
   for (var i = 0; i < c.length; i++) {
     a = c[i]
+    b = d[i]
     txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(text) > -1) {
-      a.parentElement.parentElement.parentElement.style.display = "";
+    AddressValue = b.textContent || b.innerText;
+    if (txtValue.toUpperCase().indexOf(text) > -1 || AddressValue.toUpperCase().indexOf(text) > -1 ) {
+      a.parentElement.style.display = "";
     } else {
-      a.parentElement.parentElement.parentElement.style.display = "none";
+      a.parentElement.style.display = "none";
     }
   }
 })
 
 function spinner() {
   document.getElementsByClassName("loader")[0].style.display = "block";
+}
+
+// Checked by click on row
+var tr = document.getElementsByTagName("tr"),
+    i = tr.length;
+
+while (i--) {
+    tr[i].onclick = clickTr;
+}
+
+function clickTr(event) {
+    var inputs = this.getElementsByTagName('input');
+    for (var x = 0; x < inputs.length; x++) {
+        if (inputs[x] !== event.target && inputs[x].type == 'checkbox') {
+            inputs[x].checked = !(inputs[x].checked);
+        }
+    }
+}
+
+// Checked by click on card
+var cards = document.querySelectorAll("[id^='car-card']"),
+    i = cards.length;
+
+while (i--) {
+  cards[i].onclick = clickCard;
+}
+
+function clickCard(event) {
+    var inputs = this.getElementsByTagName('input');
+    for (var x = 0; x < inputs.length; x++) {
+        if (inputs[x] !== event.target && inputs[x].type == 'checkbox') {
+            inputs[x].checked = !(inputs[x].checked);
+        }
+    }
 }
